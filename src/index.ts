841659d4db4c8a1ef7dc03b1f4af03bdb0d12984
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-10-04 17:05:15 +0300

    nice

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,33 @@
 import { JSDOM } from "jsdom";
+import { resolveObject } from "./parser";
+import util from "util";
+import { quickForEach } from "async-and-quick";
+import fs from "fs";
 
 const AllClassesIndexURL = "https://docs.oracle.com/en/java/javase/21/docs/api/allclasses-index.html";
 
-// (async () => {
-//   const { window } = await JSDOM.fromURL(AllClassesIndexURL);
-// })();
+(async () => {
+  const { window: { document } } = await JSDOM.fromURL(AllClassesIndexURL);
 
-function getNote(elm: Element, title: string): Element[] {
-  const children = [...elm.children];
-  const titleIdx = children.findIndex(i => i.textContent === title);
-  if (titleIdx === -1) return [];
-  const elms = [];
-  let i = 1;
-  while (true) {
-    let c = children[titleIdx + i];
-    if (!c || c.nodeName !== "DD") break;
-    elms.push(c);
-    i++
-  }
-  return elms;
-}
+  const objects = [];
 
-function clean(str: string) {
-  if (!str) return str;
-  return str.replace(/\s+/g, " ").trim();
-}
+  let idx = 0;
+  await quickForEach(
+    [...document.querySelectorAll('#all-classes-table .col-first a[title]') as NodeListOf<HTMLAnchorElement>],
+    async (elm, _, arr) => {
+      try {
+        const obj = await resolveObject(elm.href);
+        console.log(`[${idx++}/${arr.length}] Resolved ${obj.path}`);
+        if (idx % 50 === 0) global.gc();
+        objects.push(obj);
+      } catch (e) {
+        console.error(`Error resolving ${elm.href}`);
+        console.error(e);
+      }
+    },
+    10
+  );
 
-async function resolveObject(url: string) {
-  const { window: { document } } = await JSDOM.fromURL(url);
-
-  const modulePath = document.querySelector('main .sub-title [href*="module"]').textContent;
-  const packagePath = document.querySelector('main .sub-title [href*="package"]').textContent;
-  const fullPath = [...document.querySelectorAll('main > [title="Inheritance Tree"] .inheritance')].at(-1).textContent.replace(/<.+>/, "").trim();
-  const modifiers = document.querySelector(".class-description > .type-signature > .modifiers").textContent.split(" ").filter(Boolean);
-  const name = document.querySelector(".class-description > .type-signature > .type-name-label").textContent.replace(/<.+>/, "").trim();
-
-  const extendedClasses = [...(document.querySelectorAll('.type-signature > .extends-implements [title^="class in"]') as NodeListOf<HTMLAnchorElement>)].map(elm => {
-    const title = elm.title.split(" ");
-    return `${title.pop()}.${elm.textContent.trim()}`;
-  });
-
-  const implementedInterfaces = [...(document.querySelectorAll('.type-signature > .extends-implements [title^="interface in"]') as NodeListOf<HTMLAnchorElement>)].map(elm => {
-    const title = elm.title.split(" ");
-    return `${title.pop()}.${elm.textContent.trim()}`;
-  });
-
-  const description = clean(document.querySelector(".class-description > .block").textContent.trim());
-
-  const notesElm = document.querySelector(".class-description .notes");
-
-  const since = getNote(notesElm, "Since:")[0]?.textContent || null;
-
-  // TODO: Constructor Details, Method Details
-
-  return {
-    type: (modifiers.includes("interface") ? "Interface" : modifiers.includes("enum") ? "Enum" : modifiers.includes("record") ? "Record" : "Class") as "Class" | "Interface" | "Enum" | "Record",
-    module: modulePath,
-    package: packagePath,
-    name,
-    path: fullPath,
-    modifiers,
-    extends: extendedClasses[0] || null,
-    implements: implementedInterfaces,
-    description,
-    since,
-    constructors: [...document.querySelectorAll(".constructor-details .member-list li")].map(i => {
-      const notesElm = i.querySelector(".notes");
-      // TODO: parse constructor parameters with types
-      return {
-        modifiers: i.querySelector(".member-signature .modifiers").textContent.split(" ").filter(Boolean),
-        description: clean(i.querySelector(".block").textContent.trim()),
-        ...(notesElm ? {
-          apiNote: clean(getNote(notesElm, "API Note:")[0]?.textContent) || null,
-          throws: getNote(notesElm, "Throws:").map(t => {
-            const a = t.querySelector("code a") as HTMLAnchorElement;
-            return {
-              name: `${a.title.split(" ").pop()}.${a.textContent.trim()}`,
-              description: clean(t.textContent.split(" - ")[1])
-            }
-          })
-        } : {
-          apiNote: null,
-          throws: []
-        })
-      };
-    }),
-  }
-}
-
-resolveObject("https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/HashSet.html").then(console.log);
+  await fs.promises.writeFile("objects.json", JSON.stringify(objects, null, 2));
+  console.log("Done!");
+})();
---
+++ src/parser.ts
@@ -0,0 +1,215 @@
+import { JSDOM } from "jsdom";
+
+function getNote(elm: Element, title: string): Element[] {
+  if (!elm) return [];
+  const children = [...elm.children];
+  const titleIdx = children.findIndex(i => i.textContent === title);
+  if (titleIdx === -1) return [];
+  const elms = [];
+  let i = 1;
+  while (true) {
+    let c = children[titleIdx + i];
+    if (!c || c.nodeName !== "DD") break;
+    elms.push(c);
+    i++
+  }
+  return elms;
+}
+
+function clean(str: string) {
+  if (!str) return str;
+  return str.replace(/\s+/g, " ").trim();
+}
+
+function parseType(typeString: string) {
+  if (!typeString) return null;
+  const genericMatch = typeString.match(/([a-zA-Z0-9_]+|[?])(\[\])?(<(.+)>)?/);
+
+  if (genericMatch[3]) {
+    return {
+      name: genericMatch[1],
+      isArray: !!genericMatch[2],
+      params: genericMatch[3].split(',').map(generic => {
+        const match = generic.trim().match(/(\?|\w+)\s*(extends)?\s*(\w+)?/);
+        return {
+          name: match[1],
+          type: match[2] || null,
+          otherName: match[3] || null,
+        };
+      }),
+    };
+  }
+
+  return {
+    name: genericMatch[1],
+    isArray: !!genericMatch[2],
+    params: []
+  };
+}
+
+function parseParameters(paramsElm: Element, notesElm?: Element) {
+  if (!paramsElm) return [];
+  const result = [];
+
+  const paramString = paramsElm.textContent.replace(/[()]/g, '').trim();
+  const params = paramString.split(',').map(param => param.trim());
+
+  const paramNotes = notesElm ? getNote(notesElm, "Parameters:").map(p => ({
+    name: p.querySelector("code").textContent.trim(),
+    description: clean(p.textContent.split(" - ")[1])
+  })) : [];
+
+  const remoteParamTypes = [...paramsElm.querySelectorAll('a[title]')].map(objectLinkToPath);
+
+  params.forEach(param => {
+    const split = param.split(/\s+/);
+    const name = split.pop();
+    const typeString = split.join(' ');
+
+    const type = parseType(typeString);
+    if (!type) return;
+
+    result.push({
+      name,
+      type: {
+        ...type,
+        path: remoteParamTypes.find(i => i.endsWith(`.${type.name}`)) || null
+      },
+      description: paramNotes.find(n => n.name === name)?.description || null
+    });
+  });
+
+  return result;
+}
+
+function objectLinkToPath(elm: HTMLAnchorElement) {
+  const title = elm.title.split(" ");
+  return `${title.pop()}.${elm.textContent.trim()}`;
+}
+
+async function fetchHTMLRetry(url: string, t = 0) {
+  try {
+    return await JSDOM.fromURL(url);
+  } catch (e) {
+    await new Promise(r => setTimeout(r, t * 1000));
+    if (t >= 5) throw e;
+    return fetchHTMLRetry(url, t + 1);
+  }
+}
+
+export async function resolveObject(url: string) {
+  const dom = await fetchHTMLRetry(url);
+  const document = dom.window.document;
+
+  const modulePath = document.querySelector('main .sub-title [href*="module"]').textContent;
+  const packagePath = document.querySelector('main .sub-title [href*="package"]').textContent;
+  const modifiers = document.querySelector(".class-description > .type-signature > .modifiers").textContent.split(" ").filter(Boolean);
+  const name = document.querySelector(".class-description > .type-signature > .type-name-label").textContent.replace(/<.+>/, "").trim();
+
+  const fullPath = (() => {
+    const last = [...document.querySelectorAll('main > [title="Inheritance Tree"] .inheritance')].at(-1);
+    if (!last) return `${packagePath}.${name}`;
+    return last.textContent.replace(/<.+>/, "").trim();
+  })();
+
+  const extendedClasses = [...(document.querySelectorAll('.type-signature > .extends-implements [title^="class in"]') as NodeListOf<HTMLAnchorElement>)].map(objectLinkToPath);
+
+  const implementedInterfaces = [...(document.querySelectorAll('.type-signature > .extends-implements [title^="interface in"]') as NodeListOf<HTMLAnchorElement>)].map(objectLinkToPath);
+
+  const description = clean(document.querySelector(".class-description > .block").textContent.trim());
+
+  const notesElm = document.querySelector(".class-description .notes");
+
+  const since = getNote(notesElm, "Since:")[0]?.textContent || null;
+
+  const obj = {
+    type: (modifiers.includes("interface") ? "Interface" : modifiers.includes("enum") ? "Enum" : modifiers.includes("record") ? "Record" : "Class") as "Class" | "Interface" | "Enum" | "Record",
+    module: modulePath,
+    package: packagePath,
+    name,
+    path: fullPath,
+    modifiers,
+    extends: extendedClasses[0] || null,
+    implements: implementedInterfaces,
+    description,
+    since,
+    constructors: [...document.querySelectorAll(".constructor-details .member-list li")].map(i => {
+      const notesElm = i.querySelector(".notes");
+      const modifiersElm = i.querySelector(".member-signature .modifiers");
+      if (!modifiersElm) return;
+      const paramsElm = i.querySelector(".member-signature .parameters");
+      return {
+        modifiers: modifiersElm ? modifiersElm.textContent.split(" ").filter(Boolean) : [],
+        params: paramsElm ? parseParameters(paramsElm, notesElm) : [],
+        description: clean(i.querySelector(".block")?.textContent?.trim?.()) || null,
+        ...(notesElm ? {
+          apiNote: clean(getNote(notesElm, "API Note:")[0]?.textContent) || null,
+          throws: getNote(notesElm, "Throws:").map(t => {
+            const a = t.querySelector("code a") as HTMLAnchorElement;
+            if (!a) return null;
+            return {
+              path: `${a.title.split(" ").pop()}.${a.textContent.trim()}`,
+              description: clean(t.textContent.split(" - ")[1])
+            }
+          }).filter(Boolean)
+        } : {
+          apiNote: null,
+          throws: []
+        })
+      };
+    }).filter(Boolean),
+    methods: [...document.querySelectorAll(".method-details .member-list li")].map(i => {
+      const notesElm = i.querySelector(".notes");
+      const modifiersElm = i.querySelector(".member-signature .modifiers");
+      if (!modifiersElm) return;
+      const paramsElm = i.querySelector(".member-signature .parameters");
+      const returnsElm = i.querySelector(".member-signature .return-type");
+      const typeParamsElm = i.querySelector(".member-signature .type-parameters");
+      const nameElm = i.querySelector(".member-signature .element-name");
+      return {
+        modifiers: modifiersElm ? modifiersElm.textContent.split(" ").filter(Boolean) : [],
+        name: nameElm.textContent.trim(),
+        params: paramsElm ? parseParameters(paramsElm, notesElm) : [],
+        typeParams: typeParamsElm ? typeParamsElm.textContent.replace(/[<>]/g, "").split(",").map(i => i.trim()) : [],
+        returns: returnsElm ? (() => {
+          const type = parseType(returnsElm.textContent);
+          const remoteTypes = [...returnsElm.querySelectorAll('a[title]')].map(objectLinkToPath);
+          return {
+            ...type,
+            path: remoteTypes.find(i => i.endsWith(`.${type.name}`)) || null,
+            description: clean(getNote(notesElm, "Returns:")[0]?.textContent) || null
+          }
+        })() : null,
+        description: clean(i.querySelector(".block")?.textContent?.trim?.()) || null,
+        ...(notesElm ? {
+          apiNote: clean(getNote(notesElm, "API Note:")[0]?.textContent) || null,
+          throws: getNote(notesElm, "Throws:").map(t => {
+            const a = t.querySelector("code a") as HTMLAnchorElement;
+            if (!a) return null;
+            return {
+              path: `${a.title.split(" ").pop()}.${a.textContent.trim()}`,
+              description: clean(t.textContent.split(" - ")[1])
+            }
+          }).filter(Boolean)
+        } : {
+          apiNote: null,
+          throws: []
+        })
+      };
+    }).filter(Boolean),
+    fields: [...document.querySelectorAll(".field-details .member-list li")].map(i => {
+      const modifiersElm = i.querySelector(".member-signature .modifiers");
+      if (!modifiersElm) return;
+      const nameElm = i.querySelector(".member-signature .element-name");
+      return {
+        modifiers: modifiersElm ? modifiersElm.textContent.split(" ").filter(Boolean) : [],
+        name: nameElm.textContent.trim(),
+        description: clean(i.querySelector(".block")?.textContent?.trim?.()) || null
+      };
+    }).filter(Boolean)
+  }
+  dom.window.close();
+  // @ts-ignore
+  dom.window = null;
+  return obj;
+}
