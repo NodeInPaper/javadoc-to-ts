13eee28c742b1edf88ef1f509cc057a2d80fe766
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-10-04 11:51:25 +0300

    some progress

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,33 @@ import { JSDOM } from "jsdom";
 
 const AllClassesIndexURL = "https://docs.oracle.com/en/java/javase/21/docs/api/allclasses-index.html";
 
-(async () => {
+// (async () => {
+//   const { window } = await JSDOM.fromURL(AllClassesIndexURL);
+// })();
 
-  const { window } = await JSDOM.fromURL(AllClassesIndexURL);
-
-})();
+function getNote(elm: Element, title: string): Element[] {
+  const children = [...elm.children];
+  const titleIdx = children.findIndex(i => i.textContent === title);
+  if (titleIdx === -1) return [];
+  const elms = [];
+  let i = 1;
+  while (true) {
+    let c = children[titleIdx + i];
+    if (!c || c.nodeName !== "DD") break;
+    elms.push(c);
+    i++
+  }
+  return elms;
+}
 
 async function resolveObject(url: string) {
   const { window: { document } } = await JSDOM.fromURL(url);
 
   const modulePath = document.querySelector('main .sub-title [href*="module"]').textContent;
   const packagePath = document.querySelector('main .sub-title [href*="package"]').textContent;
-  const fullPath = [...document.querySelectorAll('main > [title="Inheritance Tree"] .inheritance')].at(-1).textContent;
+  const fullPath = [...document.querySelectorAll('main > [title="Inheritance Tree"] .inheritance')].at(-1).textContent.replace(/<.+>/, "").trim();
   const modifiers = document.querySelector(".class-description > .type-signature > .modifiers").textContent.split(" ").filter(Boolean);
-  const name = document.querySelector(".class-description > .type-signature > .type-name-label").textContent.trim();
+  const name = document.querySelector(".class-description > .type-signature > .type-name-label").textContent.replace(/<.+>/, "").trim();
 
   const extendedClasses = [...(document.querySelectorAll('.type-signature > .extends-implements [title^="class in"]') as NodeListOf<HTMLAnchorElement>)].map(elm => {
     const title = elm.title.split(" ");
@@ -27,9 +40,11 @@ async function resolveObject(url: string) {
     return `${title.pop()}.${elm.textContent.trim()}`;
   });
 
-  const description = document.querySelector(".description > .block").textContent.trim();
+  const description = document.querySelector(".class-description > .block").textContent.trim();
 
-  const since = [...document.querySelectorAll(".class-description .notes")].find(i => i.querySelector("& > dt")?.textContent === "Since:")?.querySelector("dd")?.textContent || "N/A";
+  const notesElm = document.querySelector(".class-description .notes");
+
+  const since = getNote(notesElm, "Since:")[0]?.textContent || "N/A";
 
   // TODO: Constructor Details, Method Details
 
@@ -43,6 +58,26 @@ async function resolveObject(url: string) {
     extends: extendedClasses[0] || null,
     implements: implementedInterfaces,
     description,
-    since
+    since,
+    constructors: [...document.querySelectorAll(".constructor-details .member-list li")].map(i => {
+      const notesElm = i.querySelector(".notes");
+      // TODO: parse constructor parameters with types
+      return {
+        modifiers: i.querySelector(".member-signature .modifiers").textContent.split(" ").filter(Boolean),
+        description: i.querySelector(".block").textContent.trim(),
+        ...(notesElm ? {
+          apiNote: getNote(notesElm, "API Note:")[0]?.textContent?.replace?.(/\s+/g, " ") || null,
+          throws: getNote(notesElm, "Throws:").map(t => {
+            const a = t.querySelector("code a") as HTMLAnchorElement;
+            return {
+              name: `${a.title.split(" ").pop()}.${a.textContent.trim()}`,
+              description: t.textContent.split(" - ")[1].replace(/\s+/g, " ")
+            }
+          })
+        } : {})
+      };
+    }),
   }
 }
+
+resolveObject("https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/HashSet.html").then(console.log);
